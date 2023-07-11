92115897cd6951a0181f408f516f82991f7e29b6
noahgreff <greffnoah@gmail.com>
2023-07-11 21:57:42 +0200

    added placeholder icons

--- forge.config.js
+++ forge.config.js
@@ -1,8 +1,19 @@
 module.exports = {
+	packagerConfig: {
+		icon: './public/assets/icons/app'
+	},
 	makers: [
 		{
 			name: '@electron-forge/maker-zip',
 			platforms: ['darwin', 'linux', 'win32']
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: {
+				options: {
+					icon: './public/assets/icons/app.png'
+				}
+			}
 		}
 	],
 	plugins: [
