e87a3a20fc4efbb4b12b0a948650c902133697f3
noahgreff <greffnoah@gmail.com>
2024-09-07 14:33:04 +0200

    added zip as output for macos

--- src/main/app/menu.ts
+++ src/main/app/menu.ts
@@ -9,7 +9,7 @@ const appMenu: MenuItemConstructorOptions = {
     },
     {
       label: 'Check for Updates...',
-      click: () => autoUpdater.checkForUpdates()
+      click: () => autoUpdater.checkForUpdatesAndNotify()
     },
     {
       type: 'separator'
